61a977a385808c4c5b27eb1cac5bcd00e5f090b3
Akshit Bahl <akshitbahl.ab@gmail.com>
2024-11-18 20:06:09 +0530

    second commit: added login/register changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,42 +10,48 @@ import Expenses from './Components/Expenses/Expenses';
 import { useGlobalContext } from './context/globalContext';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Team from './Components/Team/Team';
 import Contact from './Components/Contact/Contact';
 
 function App() {
   const [active, setActive] = useState(1);
   const global = useGlobalContext();
-  console.log(global);
+  const location = useLocation();
+  const showMainLayout = !['/','/login', '/register'].includes(location.pathname);
 
   const orbMemo = useMemo(() => <Orb />, []);
 
   return (
     <AppStyled bg={bg} className="App">
       {orbMemo}
-      <MainLayout>
-        <Navigation active={active} setActive={setActive} />
-        <main>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/income" element={<Income />} />
-            <Route path="/expenses" element={<Expenses />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </MainLayout>
+      {showMainLayout ? (
+        <MainLayout>
+          <Navigation active={active} setActive={setActive} />
+          <main>
+            <Routes>
+              <Route path="/income" element={<Income />} />
+              <Route path="/expenses" element={<Expenses />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+        </MainLayout>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      )}
     </AppStyled>
   );
 }
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
   main {
     flex: 1;
